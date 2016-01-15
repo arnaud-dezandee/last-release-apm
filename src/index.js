db2a11c45c8eee262b864ea152c2793f0757a0a0
Arnaud Dezandee <dezandee.arnaud@gmail.com>
2016-01-15 13:38:45 +0100

    fix(api): return empty obj if no version found

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import getHead from './head.js';
 export default function lastRelease(pluginConfig, { pkg }, callback) {
   atomVersion(pkg, (err1, version = null) => {
     if (err1) return callback(err1);
-    if (!!version) return callback(null, {});
+    if (!version) return callback(null, {});
 
     getHead(pkg, version, (err2, gitHead = null) => {
       if (err2) return callback(err2);
