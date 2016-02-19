e666323b579eeabf55f1fd4492701a6e995c2bc9
Arnaud Dezandee <dezandee.arnaud@gmail.com>
2016-02-19 12:51:34 +0100

    feat(log): add debug logs

--- src/index.js
+++ src/index.js
@@ -2,19 +2,30 @@
  * Dependencies
  */
 
+import debug from 'debug';
 import atomVersion from './version.js';
 import getHead from './head.js';
+
+/**
+ * Dependencies
+ */
+
+const log = debug('last-release-apm');
 
 /**
  * Interface
  */
 
 export default function lastRelease(pluginConfig, { pkg, options }, callback) {
+  log(options);
+
   atomVersion(pkg, (err1, version = null) => {
+    log({ version, err1 });
     if (err1) return callback(err1);
     if (!version) return callback(null, {});
 
     getHead(options, pkg, version, (err2, gitHead = null) => {
+      log({ gitHead, err2 });
       if (err2) return callback(err2);
 
       return callback(null, {
